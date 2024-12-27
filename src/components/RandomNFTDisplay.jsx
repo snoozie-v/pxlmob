9b718ca9d1596402b7c05f3c3fd846d5649c6dbe
snoozie-v <gettingsleepyz@gmail.com>
2024-12-26 22:01:52 -0600

    added tokenId to main image

--- src/components/RandomNFTDisplay.jsx
+++ src/components/RandomNFTDisplay.jsx
@@ -11,12 +11,12 @@ const RandomNFTDisplay = () => {
   const fetchRandomNFT = async () => {
     setIsLoading(true);
     setError(null);
-
+    let randomIndex;
+    
     try {
       // Generate a random number between 1 and 735 (inclusive)
-      const randomIndex = Math.floor(Math.random() * totalNFTs) + 1;
-    //   console.log('randomIndex', randomIndex)
-      // Assuming you have an API endpoint that can fetch by index or token ID
+      randomIndex = Math.floor(Math.random() * totalNFTs) + 1;
+      // Fetching NFT by token ID
       const response = await fetch(`https://arc72-voi-mainnet.nftnavigator.xyz/nft-indexer/v1/tokens?contractId=447482&tokenId=${randomIndex}`);
 
       if (!response.ok) {
@@ -24,13 +24,18 @@ const RandomNFTDisplay = () => {
       }
       const data = await response.json();
       // Assuming the 'data' contains metadata for the NFT
-      if (data.tokens[0].metadataURI) {
+      if (data.tokens.length > 0) {
+
         let metadata = {} ;
+        metadata.tokenId = data.tokens[0].tokenId
         try {
-          metadata = JSON.parse(data.tokens[0].metadata);
-          console.log('metadata', metadata, metadata.image)
-          if (metadata) {
-            setRandomNFT(metadata.image);
+          // Parse the existing metadata if available
+          const parsedMetadata = JSON.parse(data.tokens[0].metadata);
+          metadata = { ...metadata, ...parsedMetadata };
+          console.log('metadata', metadata, metadata.image, metadata.tokenId);
+  
+          if (metadata.image) {
+            setRandomNFT(metadata);
           } else {
             throw new Error('No image found in metadata');
           }
@@ -39,7 +44,7 @@ const RandomNFTDisplay = () => {
           setError('Failed to fetch or parse NFT image.');
         }
       } else {
-        throw new Error('No metadata found for this NFT');
+        throw new Error('No tokens found for this NFT');
       }
     } catch (err) {
       setError(`Failed to fetch NFT: ${err.message}`);
@@ -57,11 +62,14 @@ const RandomNFTDisplay = () => {
   return (
     <div>
       {randomNFT ? (
-        <img 
-          src={randomNFT} 
-          alt="Random NFT" 
-          style={{ width: '300px', height: '300px' }}
-        />
+        <>
+          <p>Token ID: {randomNFT.tokenId}</p>
+          <img 
+            src={randomNFT.image} 
+            alt={`NFT ${randomNFT.tokenId}`} 
+            style={{ width: '300px', height: '300px' }}
+          />
+        </>
       ) : (
         <p>No NFT image to display.</p>
       )}
